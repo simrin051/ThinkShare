505888aebe350e26d26b8316634cb274b2a95e86
simrin051 <simrinjoshi@yahoo.com>
2022-07-08 15:55:01 +0530

    included backend codebase

--- social-media/src/index.js
+++ social-media/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter as Router} from "react-router-dom";
-
+import { makeServer } from "./server";
 import { ChakraProvider } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+makeServer();
+
 root.render(
   <React.StrictMode>
     <Router>
