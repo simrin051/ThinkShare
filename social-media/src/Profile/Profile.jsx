4e9683eed37777d81066325e0afdb622dd59c8d4
simrin051 <simrinjoshi@yahoo.com>
2022-07-08 20:06:08 +0530

    profile bio

--- social-media/src/Profile/Profile.jsx
+++ social-media/src/Profile/Profile.jsx
@@ -12,12 +12,14 @@ export const Profile = () => {
             <button class="fr mt-2 p-2 follow-btn">Follow</button>
         </div>
         <div class="profile-details">
-            <div>Master Shi Heng</div>
-            <div>@mastershi</div>
-            <div class="bio">belongs to the 35th Generation of Shaolin Masters and the headmaster of the Shaolin Temple Europe</div>
-            <div class="biolink"><a href="">sample link</a></div>
-            <div class="followers">0 Followers</div>
-            <div class="following">0 Following</div>
+            <div class="username">Master Shi Heng</div>
+            <div class="userhandle">@mastershi</div>
+            <div class="userbio">belongs to the 35th Generation of Shaolin Masters and the headmaster of the Shaolin Temple Europe</div>
+            <div class="userbiolink"><i class="fa-solid fa-link"></i><a href="https://leetcode.com/Simrin_Joshi/">https://leetcode.com/Simrin_Joshi/</a></div>
+            <div class="profile-follow-container">
+                <div class="userfollowers">0 Followers</div>
+                <div class="userfollowing">0 Following</div>
+            </div>
         </div>       
     </div><FollowRequests/></div>)
 }
