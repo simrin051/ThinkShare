73628aa3446e32e02d568adcabb8fed2a046f690
simrin051 <simrinjoshi@yahoo.com>
2023-01-07 17:14:57 +0530

    profile changes

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+
+const initialState = [];
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    createPost(state, action) {
+      console.log("create post");
+      state.push(action.payload);
+    }
+  }
+})
+
+export const { postAdded } = postsSlice.actions
+export const postsSliceReducer = postsSlice.reducer;
